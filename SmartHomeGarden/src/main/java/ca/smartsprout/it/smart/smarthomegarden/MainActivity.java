ceb9254a79327fceed49db6cadbf44f67da64722
Aditi <pateladiti543@gmail.com>
2024-09-18 15:55:35 -0400

    Implemented alertdialog box on back pressed.

--- SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/MainActivity.java
+++ SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/MainActivity.java
@@ -1,17 +1,48 @@
 package ca.smartsprout.it.smart.smarthomegarden;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.KeyEvent;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    // Alert dialog box which shows up when user click on back button.
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            // Show exit confirmation dialog
+            showExitConfirmationDialog();
+            return true;  // Indicate that the event has been handled
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    public void showExitConfirmationDialog() {
+        AlertDialog.Builder builder;
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("Exit");
+        builder.setIcon(R.drawable.alert);
+        builder.setMessage("Do you want to exit the application?");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        builder.create().show();
     }
 }
