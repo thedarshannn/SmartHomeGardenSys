e2ed3a35b7814c58e6617b59ccdf4ba1737342f6
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-09-30 18:40:20 -0400

    Created Fragments for required screens.

---
+++ SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/ui/fragments/SensorFragment.java
@@ -0,0 +1,33 @@
+package ca.smartsprout.it.smart.smarthomegarden.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ca.smartsprout.it.smart.smarthomegarden.R;
+
+
+public class SensorFragment extends Fragment {
+
+
+    public SensorFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sensor, container, false);
+    }
+}
