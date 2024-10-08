52ef28eb35f28c269e41fba69ec96f99a8b9ba56
Birava Prajapati <139030323+Birava-20@users.noreply.github.com>
2024-10-08 19:23:04 -0400

    Implement the functionality to lock the screen orientation.

--- SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/ui/SettingsActivity.java
+++ SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/ui/SettingsActivity.java
@@ -1,9 +1,13 @@
 package ca.smartsprout.it.smart.smarthomegarden.ui;
 
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import ca.smartsprout.it.smart.smarthomegarden.R;
@@ -27,10 +31,53 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
+    public static class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            // Set up the Log Out option, no functionality yet
+            Preference logoutPreference = findPreference("logout");
+            if (logoutPreference != null) {
+                logoutPreference.setOnPreferenceClickListener(preference -> {
+                    // Log Out click event, functionality can be added later
+                    return true;
+                });
+            }
+        }
+
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, @Nullable String key) {
+            if (key.equals("screen_orientation")) {
+                String orientation = sharedPreferences.getString(key, "auto");
+
+                // Apply the selected orientation
+                switch (orientation) {
+                    case "portrait":
+                        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                        break;
+                    case "landscape":
+                        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                        break;
+                    default:
+                        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+                        break;
+                }
+            }
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences()
+                    .registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences()
+                    .unregisterOnSharedPreferenceChangeListener(this);
         }
     }
 }
