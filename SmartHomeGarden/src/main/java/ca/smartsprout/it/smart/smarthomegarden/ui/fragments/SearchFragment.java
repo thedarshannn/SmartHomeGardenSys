abf45949cea6353a55369e08b5a0fb153a9fd6e4
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-10-31 15:33:02 -0400

    Set Up the RecyclerView in the Fragment.

--- SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/ui/fragments/SearchFragment.java
+++ SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/ui/fragments/SearchFragment.java
@@ -12,15 +12,27 @@ package ca.smartsprout.it.smart.smarthomegarden.ui.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 import ca.smartsprout.it.smart.smarthomegarden.R;
+import ca.smartsprout.it.smart.smarthomegarden.ui.adapter.PlantAdapter;
+import ca.smartsprout.it.smart.smarthomegarden.viewmodels.PlantViewModel;
 
 
 public class SearchFragment extends Fragment {
+
+    private PlantViewModel plantViewModel;
+    private PlantAdapter plantAdapter;
+
 
     public SearchFragment() {
         // Required empty public constructor
@@ -36,7 +48,24 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.searchRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+        // Set up the adapter with an empty list initially
+        plantAdapter = new PlantAdapter(new ArrayList<>());
+        recyclerView.setAdapter(plantAdapter);
+
+        // Set up ViewModel and observe LiveData
+        plantViewModel = new ViewModelProvider(this).get(PlantViewModel.class);
+        plantViewModel.getPlantList().observe(getViewLifecycleOwner(), plants -> {
+            // Update the adapter's data when the plant list changes
+            plantAdapter.updatePlantList(plants);
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        return view;
     }
 }
