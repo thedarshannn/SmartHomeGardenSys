ac236eff2590a9b254c36d1cc2c965cbcaf3f570
ZeelPatel6282 <zeel12803@gmail.com>
2024-10-30 22:23:35 -0400

    Added Gmail functionality in Register Screen

--- SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/ui/RegistrationActivity.java
+++ SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/ui/RegistrationActivity.java
@@ -17,22 +17,36 @@ import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
-
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import ca.smartsprout.it.smart.smarthomegarden.MainActivity;
 import ca.smartsprout.it.smart.smarthomegarden.R;
 import ca.smartsprout.it.smart.smarthomegarden.viewmodels.AuthViewModel;
 
+
+
+
+
 public class RegistrationActivity extends AppCompatActivity {
-
+    private static final int RC_SIGN_IN = 9001;
     private EditText emailInput, passwordInput;
-    private Button registerButton;
+    private Button registerButton,googleSignInButton;
     private AuthViewModel authViewModel;
-
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +56,7 @@ public class RegistrationActivity extends AppCompatActivity {
         emailInput = findViewById(R.id.editTextEmail1);
         passwordInput = findViewById(R.id.editTextPassword1);
         registerButton = findViewById(R.id.button2);
+        googleSignInButton = findViewById(R.id.googleButton);
         passwordInput.setFilters(new InputFilter[]{new InputFilter.LengthFilter(10)});
         // Initialize ViewModel
 
@@ -63,9 +78,17 @@ public class RegistrationActivity extends AppCompatActivity {
 
 
         authViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
+        mAuth = FirebaseAuth.getInstance();
 
+        // Configure Google Sign-In options
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.webclient)) // Ensure this matches your client ID in google-services.json
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         // Set click listener for register button
         registerButton.setOnClickListener(v -> registerUser());
+        googleSignInButton.setOnClickListener(v -> signInWithGoogle());
     }
 
     private void registerUser() {
@@ -82,6 +105,56 @@ public class RegistrationActivity extends AppCompatActivity {
 
             // Register user and observe result
             authViewModel.registerUser(email, password).observe(this, this::handleRegistrationResult);
+        }
+    }
+
+    private void signInWithGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleGoogleSignInResult(task);
+        }
+    }
+
+    private void handleGoogleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            firebaseAuthWithGoogle(account.getIdToken());
+        } catch (ApiException e) {
+            Toast.makeText(this, "Google Sign-In failed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Toast.makeText(RegistrationActivity.this, "Google Sign-In successful!", Toast.LENGTH_SHORT).show();
+
+                    updateUI(user);
+                } else {
+                    Toast.makeText(RegistrationActivity.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+            }
+        });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+            startActivity(intent);
         }
     }
 
