b07201c6d404b882651a606acc44a1b995f50148
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-11-15 00:26:31 -0500

    Added the PlantViewModel for parsing JSON response.

--- SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/viewmodels/PlantViewModel.java
+++ SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/viewmodels/PlantViewModel.java
@@ -8,20 +8,35 @@
  */
 package ca.smartsprout.it.smart.smarthomegarden.viewmodels;
 
-// PlantViewModel.java
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.List;
 
+import ca.smartsprout.it.smart.smarthomegarden.BuildConfig;
 import ca.smartsprout.it.smart.smarthomegarden.data.model.Plant;
+import ca.smartsprout.it.smart.smarthomegarden.data.model.PlantResponse;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class PlantViewModel extends ViewModel {
     private final MutableLiveData<List<Plant>> plantList = new MutableLiveData<>();
+    private static final String API_URL = "https://perenual.com/api/species-list?key=" + BuildConfig.API_KEY;
+    private final OkHttpClient client = new OkHttpClient();
+    private final Gson gson = new Gson();
 
     public PlantViewModel() {
-        loadPlants(); // Load initial data
+        loadPlants();
     }
 
     public LiveData<List<Plant>> getPlantList() {
@@ -29,6 +44,36 @@ public class PlantViewModel extends ViewModel {
     }
 
     private void loadPlants() {
-        // Set the dummy data to LiveData
+        // Build the request
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .build();
+
+        // Make the API call
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful() && response.body() != null) {
+                    // Parse JSON response
+                    String jsonResponse = response.body().string();
+                    Type responseType = new TypeToken<PlantResponse>() {}.getType();
+                    PlantResponse plantResponse = gson.fromJson(jsonResponse, responseType);
+
+
+                    // Update the LiveData with the data
+                    if (plantResponse != null) {
+                        plantList.postValue(plantResponse.getData());
+                    }
+                } else {
+                    plantList.postValue(null);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+                plantList.postValue(null);  // Set to null on failure
+            }
+        });
     }
 }
