06d2cee65bf085b2f555347e5eca9658252f6c28
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-11-26 18:18:02 -0500

    Add NetworkUtils class to check internet connectivity.

--- SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/ui/OfflineActivity.java
+++ SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/ui/OfflineActivity.java
@@ -1,12 +1,11 @@
-package ca.smartsprout.it.smart.smarthomegarden;
+package ca.smartsprout.it.smart.smarthomegarden.ui;
 
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import ca.smartsprout.it.smart.smarthomegarden.R;
 
 public class OfflineActivity extends AppCompatActivity {
 
---
+++ SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/utils/NetworkUtils.java
@@ -0,0 +1,42 @@
+/**
+ * Smart Sprout
+ * Members:
+ * 1. Aditi Patel, n01525570, CENG322-RCB
+ * 2. Birava Prajapati, n01579924, CENG322-RCA
+ * 3. Darshankumar Prajapati, n01584247, CENG322-RCB
+ * 4. Zeel Patel, n01526282, CENG322-RCB
+ */
+
+package ca.smartsprout.it.smart.smarthomegarden.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+
+
+public class NetworkUtils {
+
+    /**
+     * Check if the device is connected to the internet.
+     *
+     * @param context The context of the application.
+     * @return True if the device is connected to the internet, false otherwise.
+     */
+    public static boolean isInternetAvailable(Context context) {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager != null) {
+            Network network = connectivityManager.getActiveNetwork();
+            if (network != null) {
+                NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+                return networkCapabilities != null &&
+                        (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET));
+            }
+        }
+        return false;
+    }
+}
