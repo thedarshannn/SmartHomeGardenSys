1d0281cd17247210cc2e731742dffac3e79cb4a8
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2025-03-07 22:35:53 -0500

    Added PairActivity.

---
+++ SmartHomeGarden/src/main/java/ca/smartsprout/it/smart/smarthomegarden/ui/PairActivity.java
@@ -0,0 +1,19 @@
+package ca.smartsprout.it.smart.smarthomegarden.ui;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import ca.smartsprout.it.smart.smarthomegarden.R;
+
+public class PairActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
